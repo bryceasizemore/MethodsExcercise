585ad3433187095ffd4a698e1ad972967307957b
bryce sizemore <brxccce@outlook.com>
2023-05-17 20:58:43 -0700

    Add project files.

---
+++ MethodsExcercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //--Excercise 1
+            //Name: Bryce
+           //Favorite Color: Purple 
+          //Favorite Animal: snake
+         //Favorite Band:  The Velvet Underground 
+
+            Console.WriteLine("Hello - what is your first name?")
+                var userName = Console.ReadLine();
+
+            Console.WriteLine("Hello - what is your first name?")
+                var userName = Console.ReadLine();
+
+            Console.WriteLine("Hello - what is your first name?")
+                var userName = Console.ReadLine();
+
+            Console.WriteLine("Hello - what is your first name?")
+                var userName = Console.ReadLine();
+
+
+
+        }
+    }
+}
